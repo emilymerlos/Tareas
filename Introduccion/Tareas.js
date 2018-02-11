869366667ab1056bbee996b8d1db86efacc0e187
Emily Merlos <emilygarmer@gmail.com>
2018-02-10 19:59:55 -0800

    se agrego la carpeta con el archivo de variables

---
+++ Introduccion/Tareas.js
@@ -0,0 +1,54 @@
+var mes = 10;
+
+console.log(mes);
+
+document.write(mes);
+
+
+switch(mes){
+  case 1: 
+    alert("es invierno"); 
+    break;
+  case 2: 
+    alert("es invierno"); 
+    break;
+  case 3:
+    alert("es primavera");
+    break;
+  case 4:
+    alert("es primavera");
+    break;
+  case 5: 
+    alert("es primavera");
+    break;
+  case 6: 
+    alert ("es verano");
+    break;
+  case 7: 
+    alert("es verano");
+    break;
+  case 8: 
+    alert("es verano");
+    break;
+  case 9: 
+    alert("es otoño");
+    break;
+  case 10: 
+    alert("es otoño");
+    break;
+  case 11: 
+    alert("es otoño");
+    break;
+  case 12: 
+    alert("es invierno"); 
+    break;
+  default: 
+    alert("no es un mes valido");
+    break;
+}
+
+var nombre = "Emily Anabella Garcia Merlos";
+alert(nombre);
+var carnet = "18009133";
+alert(carnet);
+
